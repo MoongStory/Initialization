f2759fe662f46480448922c309a30d2536d11560
Moong <moongstory@gmail.com>
2021-12-28 23:17:22 +0900

    const 누락된 곳 const 추가

--- Initialization/Initialization.cpp
+++ Initialization/Initialization.cpp
@@ -10,9 +10,9 @@ MOONG::INITIALIZATION::Initialization::Initialization(const CStringA fail_string
 
 
 
-DWORD MOONG::INITIALIZATION::Initialization::Write(CStringA file_path, CStringA app_name, CStringA key_name, CStringA value) const
+DWORD MOONG::INITIALIZATION::Initialization::Write(const CStringA file_path, const CStringA app_name, const CStringA key_name, const CStringA value) const
 {
-	if (WritePrivateProfileStringA(app_name.GetBuffer(), key_name.GetBuffer(), value.GetBuffer(), file_path.GetBuffer()) == 0)
+	if (WritePrivateProfileStringA(app_name.GetString(), key_name.GetString(), value.GetString(), file_path.GetString()) == 0)
 	{
 		return GetLastError();
 	}
@@ -20,7 +20,7 @@ DWORD MOONG::INITIALIZATION::Initialization::Write(CStringA file_path, CStringA 
 	return EXIT_SUCCESS;
 }
 
-DWORD MOONG::INITIALIZATION::Initialization::Write(CStringA file_path, CStringA app_name, CStringA key_name, int value) const
+DWORD MOONG::INITIALIZATION::Initialization::Write(const CStringA file_path, const CStringA app_name, const CStringA key_name, int value) const
 {
 	CStringA convert_string;
 	convert_string.Format("%d", value);
@@ -30,12 +30,12 @@ DWORD MOONG::INITIALIZATION::Initialization::Write(CStringA file_path, CStringA 
 
 
 
-DWORD MOONG::INITIALIZATION::Initialization::Read(CStringA file_path, CStringA app_name, CStringA key_name, char* output, DWORD length_output) const
+DWORD MOONG::INITIALIZATION::Initialization::Read(const CStringA file_path, const CStringA app_name, const CStringA key_name, char* output, DWORD length_output) const
 {
-	return GetPrivateProfileStringA(app_name.GetBuffer(), key_name.GetBuffer(), this->Get_fail_string().GetString(), output, length_output, file_path.GetBuffer());
+	return GetPrivateProfileStringA(app_name.GetString(), key_name.GetString(), this->Get_fail_string().GetString(), output, length_output, file_path.GetString());
 }
 
-DWORD MOONG::INITIALIZATION::Initialization::Read(CStringA file_path, CStringA app_name, CStringA key_name, wchar_t* output, DWORD length_output) const
+DWORD MOONG::INITIALIZATION::Initialization::Read(const CStringA file_path, const CStringA app_name, const CStringA key_name, wchar_t* output, DWORD length_output) const
 {
 	char* buf = new char[length_output];
 
@@ -50,9 +50,9 @@ DWORD MOONG::INITIALIZATION::Initialization::Read(CStringA file_path, CStringA a
 	return return_value;
 }
 
-unsigned int MOONG::INITIALIZATION::Initialization::Read(CStringA file_path, CStringA app_name, CStringA key_name) const
+unsigned int MOONG::INITIALIZATION::Initialization::Read(const CStringA file_path, const CStringA app_name, const CStringA key_name) const
 {
-	return GetPrivateProfileIntA(app_name.GetBuffer(), key_name.GetBuffer(), this->Get_fail_value(), file_path.GetBuffer());
+	return GetPrivateProfileIntA(app_name.GetString(), key_name.GetString(), this->Get_fail_value(), file_path.GetString());
 }
 
 
@@ -67,7 +67,7 @@ unsigned int MOONG::INITIALIZATION::Initialization::Get_fail_value() const
 	return this->fail_value_;
 }
 
-void MOONG::INITIALIZATION::Initialization::Set_fail_string(CStringA fail_string)
+void MOONG::INITIALIZATION::Initialization::Set_fail_string(const CStringA fail_string)
 {
 	this->fail_string_ = fail_string;
 }
